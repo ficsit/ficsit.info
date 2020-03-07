bc2198907082a7d241c4475d49d8aef3a162e68b
Ian MacLeod <ian@nevir.net>
2020-03-07 00:01:53 -0800

    Embetterer tagline

--- web/pages/Solver/SolverPage.tsx
+++ web/pages/Solver/SolverPage.tsx
@@ -1,16 +1,28 @@
 import { css } from '@emotion/core';
 
 import { Section } from '~/components/Section'
-import { sizing } from '~/style';
+import { sizing, colors } from '~/style';
 
 const rootStyles = css({
   padding: sizing.sectionPadding,
 });
 
+const taglineStyles = css({
+  fontSize: sizing.FontSize.Small,
+  color: colors.Dark.N500,
+});
+
 export function SolverPage() {
   return (
     <article css={rootStyles}>
-      <Section title={<h1>Embetterer™</h1>}>
+      <Section 
+        title={
+          <span>
+            <h1>Embetterer™</h1>
+            <span css={taglineStyles}>Embetter your production line with The Embetterer™!</span>
+          </span>
+        }
+      >  
         <div>
           <h3>Production Targets</h3>
         </div>
